0ad10fe0a8a743d7b488276576518ad4a1848044
ingelectronicadj <ingelectronicadj@gmail.com>
2017-09-10 19:38:22 -0500

    se añade la representacion de histograma de la imagen original a color - formato rgb

--- transformacionEspacial/transformacionEspacial.m
+++ transformacionEspacial/transformacionEspacial.m
@@ -1,16 +1,21 @@
 close all; clear all; clc;
 %TRANSFORMACION GEOMETRICA BILINEAL
 
-%im1 = imread('images/Automovil023.png');
+im1 = imread('images/Automovil023.png');
 %im1 = imread('images/placa_014.png');
 %im1 = imread('images/Automovil032.png');
-im1 = imread('images/Automovil006.png');
+%im1 = imread('images/Automovil006.png');
 
 figure(1), imshow(im1), title('Imagen Original')
+h1r = imhist(im1(:,:,1));
+h1g = imhist(im1(:,:,2));
+h1b = imhist(im1(:,:,3));
+figure(2), plot(0:255,h1r,'r');hold on; plot(0:255,h1g,'g'); plot(0:255,h1b,'b'); grid on
+title('Histograma de de la imagen Original a Color - Formato RGB')
 
 % Seleccionamos los vertices (x,y) de la placa en vista lateral de la Imagen Original
-%y = [230,223,268,281]';
-%x = [329,396,393,328]';
+y = [230,223,268,281]';
+x = [329,396,393,328]';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %y = [135,133,188,192]';
 %x = [267,359,356,265]';
@@ -18,8 +23,8 @@ figure(1), imshow(im1), title('Imagen Original')
 % y = [146,157,196,176]';
 % x = [268,311,300,258]';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-y = [222,231,280,264]';
-x = [103,174,169,98]';
+%y = [222,231,280,264]';
+%x = [103,174,169,98]';
 
 yp = [1,1,160,160]'; %Asignamos un tamano para la imagen final
 xp = [1,330,330,1]';
@@ -35,11 +40,11 @@ bp = Mp^(-1)*y;
 % transformada puede quedar con un valor que no es entero por eso el round
 for m=1:160, 
     for n=1:330, 
-        im2(m,n)=im1(round(bp'*[1;n;m;n*m]), round(ap'*[1;n;m;n*m]));
+        im3(m,n)=im1(round(bp'*[1;n;m;n*m]), round(ap'*[1;n;m;n*m]));
     end; 
 end;
 
-figure(2), imshow(im2); title('Transformacion Vista Frontal')
+figure(3), imshow(im3); title('Transformacion Vista Frontal')
 
 % Para reconocer texto mediante reconocimiento optico de caracteres
 % https://www.mathworks.com/help/vision/ref/ocr.html?s_tid=srchtitle

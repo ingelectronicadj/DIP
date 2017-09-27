10fc030498b15d6b31dcc727cddf279edecbc1ec
ingelectronicadj <ingelectronicadj@gmail.com>
2017-09-27 18:25:13 -0500

    Se añade rotulo de figura pero hay que corregirla

--- deteccionFigurasGeometricas/deteccion_figuras.m
+++ deteccionFigurasGeometricas/deteccion_figuras.m
@@ -10,37 +10,36 @@ im2=imdilate(im1,se); % dilatar aumenta tamano
 im3=im2-im1;
 %im4=xor(im2,im1);la misma cosa
 im4=bwlabel(im3); %Sirve para rotular la imagen
-%figure(3), imshow(im4); title('Bordes')
+%figure(3), imshow(im4); title('Bordes con objetos rotulados')
 
 %PUNTOS SEMILLA
-Xk = false(size(im3)); %Se define fondo logico en falso
+Xk = zeros(size(im3)); %Se define fondo logico en falso
 % Se invierte la pareja ordenada x,y por y,x
-Xk(89,74) = true; %figura 1
-Xk(75,171) = true; %figura 2
-Xk(89,301) = true; %figura 3
-Xk(90,425) = true; %figura 4
-Xk(188,67) = true; %figura 5
-Xk(201,195) = true; %figura 6
-Xk(199,330) = true; %figura 7
-Xk(221,448) = true; %figura 8
-Xk(310,96) = true; %figura 9
-Xk(303,206) = true; %figura 10
-Xk(304,301) = true; %figura 11
-Xk(324,405) = true; %figura 12
+Xk(89,74) = 250; %figura 1
+Xk(75,171) = 250; %figura 2
+Xk(89,301) = 250; %figura 3
+Xk(90,425) = 250; %figura 4
+Xk(188,67) = 250; %figura 5
+Xk(201,195) = 250; %figura 6
+Xk(199,330) = 250; %figura 7
+Xk(221,448) = 250; %figura 8
+Xk(310,96) = 250; %figura 9
+Xk(303,206) = 250; %figura 10
+Xk(304,301) = 250; %figura 11
+Xk(324,405) = 250; %figura 12
 
-% Xk(1:64,:)=[];%elimina las 64 filas primeras de la imagen para conteplar el tamaño de 384px
+im5 = bitor(im3,Xk);
+figure(1), imshow(im5); title('Bordes Externos y Puntos Semilla'); 
 
-figure(), imshow(bitor(im3,Xk)); title('Bordes Externos y Puntos Semilla'); 
-
-[m,n] = size(im3);
+[m,n] = size(im5);
 for m=1:m
     for n=1:n
-        if(im3(m,n)==true)
+        if(im5(m,n)==250)
            %Xk = false;
-           %text(m,n,'Figura', 'color', [0.3 0.6 1], 'Fontname', 'Comic');
+           text(m,n,'Figura', 'color', [0.3 0.6 1], 'Fontname', 'Comic');
         end
     end
 end
-figure(), imshow(im3)
+
 
 
